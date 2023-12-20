c2d95aa38c00dfaddf74ae3498078bf5ea687b76
jrycw <jerry@ycwu.space>
2023-12-20 22:37:09 +0800

    update tests for three-digits-radio

--- projects/three_digits_radio/test_three_digits_radio.py
+++ projects/three_digits_radio/test_three_digits_radio.py
@@ -1,32 +1,71 @@
 import pytest
 from textual.widgets import Input
-from three_digits_radio import RadioSelect, ThreeDigitsRadioApp
+from three_digits_radio import RadioSelect, RadioSet, ThreeDigitsRadioApp
 
 
 @pytest.mark.asyncio
 async def test_initial_input():
-    app = ThreeDigitsRadioApp()
-    async with app.run_test() as pilot:
+    async with ThreeDigitsRadioApp().run_test() as pilot:
         await pilot.click(Input)
-        assert app.query_one(Input).value == "000"
+        assert pilot.app.query_one(Input).value == "000"
 
 
 @pytest.mark.asyncio
 async def test_input_changed():
-    app = ThreeDigitsRadioApp()
-    async with app.run_test() as pilot:
+    async with ThreeDigitsRadioApp().run_test() as pilot:
         test_value = "234"
-        app.query_one(Input).value = test_value
+        pilot.app.query_one(Input).value = test_value
         await pilot.click(Input)
-        assert app.query_one(Input).value == test_value
-
-        for rs, digit in zip(app.query(RadioSelect), test_value):
+        assert pilot.app.query_one(Input).value == test_value
+        for rs, digit in zip(pilot.app.query(RadioSelect), test_value):
             assert rs.value == int(digit)
 
 
-@pytest.mark.skip(reason="need to figure it out how to click the specific radio button")
 @pytest.mark.asyncio
-async def test_radio_changed():
-    app = ThreeDigitsRadioApp()
-    async with app.run_test() as pilot:
-        test_value = [2, 3, 4]
+async def test_input_changed_too_large():
+    async with ThreeDigitsRadioApp().run_test() as pilot:
+        test_value = "1234"
+        pilot.app.query_one(Input).value = test_value
+        await pilot.click(Input)
+        assert pilot.app.query_one(Input).value == test_value
+        for rs in pilot.app.query(RadioSelect):
+            assert rs.value == 9
+
+
+@pytest.mark.asyncio
+async def test_input_changed_too_small():
+    async with ThreeDigitsRadioApp().run_test() as pilot:
+        test_value = "-1"
+        pilot.app.query_one(Input).value = test_value
+        await pilot.click(Input)
+        assert pilot.app.query_one(Input).value == test_value
+        for rs in pilot.app.query(RadioSelect):
+            assert rs.value == 0
+
+
+@pytest.mark.asyncio
+async def test_invalid_input_changed():
+    async with ThreeDigitsRadioApp().run_test() as pilot:
+        test_value = "invalid"
+        pilot.app.query_one(Input).value = test_value
+        await pilot.click(Input)
+        assert pilot.app.query_one(Input).value == test_value
+        for rs in pilot.app.query(RadioSelect):
+            assert rs.value == 0
+
+
+@pytest.mark.parametrize("test_value", [(0, 0, 0), (2, 3, 4), (7, 8, 9), (9, 9, 9)])
+@pytest.mark.asyncio
+async def test_radio_changed(test_value):
+    """
+    https://github.com/Textualize/textual/blob/main/tests/toggles/test_radioset.py
+    """
+    async with ThreeDigitsRadioApp().run_test() as pilot:
+        for rs, digit in zip(pilot.app.query(RadioSet), test_value):
+            rs._nodes[digit].toggle()
+        await pilot.pause()
+
+        for rs, digit in zip(pilot.app.query(RadioSelect), test_value):
+            assert rs.value == digit
+
+        assert pilot.app.query_one(Input).value == "".join(map(str, test_value))
